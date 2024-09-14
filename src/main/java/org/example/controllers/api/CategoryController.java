a51ab061e4d435df08d3b1b155de7c1cda7d7b7a
Irafedoruk <irrnika@gmail.com>
2024-09-14 18:13:36 +0300

    Initial

--- src/main/java/org/example/controllers/api/CategoryController.java
+++ src/main/java/org/example/controllers/api/CategoryController.java
@@ -2,19 +2,19 @@ package org.example.controllers.api;
 
 import lombok.AllArgsConstructor;
 import org.example.dto.category.CategoryCreateDTO;
+import org.example.dto.category.CategoryItemDTO;
 import org.example.mapper.CategoryMapper;
+import org.example.model.CategoryEntity;
 import org.example.repo.CategoryRepository;
 import org.example.service.FileSaveFormat;
 import org.example.storage.StorageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -39,4 +39,11 @@ public class CategoryController {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("")
+    public ResponseEntity<List<CategoryItemDTO>> getAllCategories() {
+        var categories = categoryMapper.toDto(categoryRepository.findAll());
+        return ResponseEntity.ok(categories);
+    }
+
 }
